8aa5b9596fc0bed5e11072eda57979456edd64c9
Will <vladulj@list.ru>
2022-10-20 19:01:43 +0300

    Job

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp50
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ваша сумма вклада: ");
+            double n = Convert.ToDouble(Console.ReadLine());
+            if (n < 100)
+            {
+                n = (n/100)*5;
+                Console.WriteLine(n);
+            }
+            else
+            {
+                if (n >= 100 && n <= 200)
+                {
+                    n = (n/100)*7;
+                    Console.WriteLine(n);
+                }
+                else
+                {
+                    if (n > 200)
+                    {
+                        n = (n/100)*10;
+                        Console.WriteLine(n);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ошибка");
+                    }
+                }
+            }
+
+        }
+    }
+}
